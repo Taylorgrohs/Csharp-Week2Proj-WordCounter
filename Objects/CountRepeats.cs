ada035e8e38dc54afde035af0882ac3f45b45dc9
Taylor <taylorgrohs@gmail.com>
2016-02-19 08:56:18 -0800

    add fail test 2

--- Objects/CountRepeats.cs
+++ Objects/CountRepeats.cs
@@ -8,14 +8,14 @@ namespace RepeatCounter
     public string Count(string wordToCount, string sentenceToCheck)
     {
       string word = wordToCount.ToLower();
-      string[] sentence = sentenceToCheck.ToLower().Split(' ');
+      string[] sentence = sentenceToCheck.Split(' ');
       int repeatNum = 0;
 
       foreach (string i in sentence)
       {
         if (word == i)
         {
-        repeatNum = repeatNum+1;
+        repeatNum = repeatNum + 1;
         }
       }
       return wordToCount + " was repeated in " + sentenceToCheck + " " + repeatNum + " time(s).";
--- Tests/CountRepeatsTest.cs
+++ Tests/CountRepeatsTest.cs
@@ -12,5 +12,11 @@ namespace RepeatCounter
       Sentence newSentence = new Sentence();
       Assert.Equal("hello was repeated in hello 1 time(s).", newSentence.Count("hello", "hello"));
     }
+    [Fact]
+    public void Count_Hello_Equals_Lowercase()
+    {
+      Sentence newSentence = new Sentence();
+      Assert.Equal("hello was repeated in HeLLo 1 time(s).", newSentence.Count("hello", "HeLLo"));
+    }
   }
 }
